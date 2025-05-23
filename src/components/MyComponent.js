4fdbe720fc1248bc85cd37339aae4e803eb5bd8b
haanhtuan1710 <hatuan17102003@gmail.com>
2025-05-23 13:35:54 +0700

    dom

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,14 +11,25 @@ class MyComponent extends React.Component {
         age: 18
     };
 
+    handleClick(event){
+        //console.log("click me")
+        console.log("My name is", this.state.name)
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX)
+    }
+
     //jsx, ham render ke thua tu react.component, cho phep code html trong js
-    render(){
+    render() {
         return (
 
-            <div> 
-                My name is {this.state.name} and 
+            <div>
+                My name is {this.state.name} and
                 I'm from {this.state.address}
-                
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <button onClick={this.handleClick}>Click me</button>
+
             </div>
         );
     }
