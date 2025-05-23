bb6a7e336e235a447273870fe60930c20aa37304
haanhtuan1710 <hatuan17102003@gmail.com>
2025-05-23 13:09:33 +0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,12 +5,20 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'hat',
+        address: 'hn',
+        age: 18
+    };
+
     //jsx, ham render ke thua tu react.component, cho phep code html trong js
     render(){
         return (
 
-            <div>my first component
-                {Math.random()}
+            <div> 
+                My name is {this.state.name} and 
+                I'm from {this.state.address}
+                
             </div>
         );
     }
