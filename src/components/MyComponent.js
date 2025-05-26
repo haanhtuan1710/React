7583bff3ad4dda9adb9f005abd6d5ea264725803
haanhtuan1710 <hatuan17102003@gmail.com>
2025-05-26 08:22:44 +0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,13 +11,20 @@ class MyComponent extends React.Component {
         age: 18
     };
 
-    handleClick(event){
-        //console.log("click me")
-        console.log("My name is", this.state.name)
+    handleClick(event) {
+        console.log("click me")
+        //console.log("Random", Math.floor(Math.random()*100 + 1))
+
+        // merge state => react class
+        this.setState({
+            name: 'hat1',
+            age: Math.floor(Math.random()*100 + 1)
+        })
+
     }
 
     handleOnMouseOver(event) {
-        console.log(event.pageX)
+        //console.log(event.pageX)
     }
 
     //jsx, ham render ke thua tu react.component, cho phep code html trong js
@@ -26,9 +33,9 @@ class MyComponent extends React.Component {
 
             <div>
                 My name is {this.state.name} and
-                I'm from {this.state.address}
+                I'm from {this.state.age}
                 <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
-                <button onClick={this.handleClick}>Click me</button>
+                <button onClick={(event) => this.handleClick(event)}>Click me</button>
 
             </div>
         );
