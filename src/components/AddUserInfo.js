0277bacaf2d228ebf674854e08dab750c7fc4523
haanhtuan1710 <hatuan17102003@gmail.com>
2025-05-27 20:32:29 +0700

    function as props

--- src/components/AddUserInfo.js
+++ src/components/AddUserInfo.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-class UserInfo extends React.Component {
+class AddUserInfo extends React.Component {
 
     state = {
-        name: 'hat',
-        address: 'hn',
-        age: 18
+        name: '',
+        address: '',
+        age: ''
     };
 
     handleOnChangeInput = (event) => {
@@ -23,7 +23,11 @@ class UserInfo extends React.Component {
 
     handleOnSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state)
+        this.props.handleAddNewUser({
+            id : Math.floor(Math.random()*100 + 1) + '-random',
+            name: this.state.name,
+            age : this.state.age
+        });
     }
 
     render() {
@@ -52,4 +56,4 @@ class UserInfo extends React.Component {
 
 }
 
-export default UserInfo;
+export default AddUserInfo;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,16 +1,24 @@
 
 import React from "react";
-import UserInfo from "./UserInfo";
+import AddUserInfo from "./AddUserInfo";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
 
-    state={
+    state = {
         listUser: [
-            {id: 1, name : "hat" ,age : "16"},
-            {id: 2, name : "hat1" ,age : "18"},
-            {id: 3, name : "hat2" ,age : "69"},
+            { id: 1, name: "hat", age: "16" },
+            { id: 2, name: "hat1", age: "18" },
+            { id: 3, name: "hat2", age: "69" },
         ]
+    }
+
+    handleAddNewUser = (userObj) => {
+        let listUserClone = [...this.state.listUser];
+        
+        // this.setState({
+        //     listUser:[userObj, ...this.state.listUser]
+        // })
     }
 
     //jsx, ham render ke thua tu react.component, cho phep code html trong js
@@ -18,10 +26,14 @@ class MyComponent extends React.Component {
     render() {
         return (
             <div>
-                <UserInfo />
+                <AddUserInfo 
+                handleAddNewUser={this.handleAddNewUser} // them dau dong mo ngoac se thuc thi cau lenh chu khong phai gan nua
+                />
                 <br></br>
-                <DisplayInfor 
-                listUser={this.state.listUser}/>
+                <DisplayInfor
+                    listUser={this.state.listUser}
+                    
+                />
             </div>
         );
     }
