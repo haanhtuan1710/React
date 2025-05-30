4743a69412ce4a226cc8c3e0685288c778f879cb
haanhtuan1710 <hatuan17102003@gmail.com>
2025-05-30 16:33:59 +0700

    side bar css

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -11,6 +11,8 @@ import {
 import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
 import sidebarBg from '../../assets/bg2.jpg';
 
+import { DiReact } from 'react-icons/di';
+import { MdDashboard } from 'react-icons/md';
 
 const SideBar = (props) => {
     const { image, collapsed, toggled, handleToggleSidebar } = props;
@@ -36,6 +38,7 @@ const SideBar = (props) => {
                             whiteSpace: 'nowrap',
                         }}
                     >
+                        <DiReact size={'3em'} color='blue' />
                         Hat1710
                     </div>
                 </SidebarHeader>
@@ -43,21 +46,19 @@ const SideBar = (props) => {
                 <SidebarContent>
                     <Menu iconShape="circle">
                         <MenuItem
-                            icon={<FaTachometerAlt />}
-                            suffix={<span className="badge red">New</span>}
+                            icon={<MdDashboard />}
                         >
                             Dash board
                         </MenuItem>
-                        <MenuItem icon={<FaGem />}> Components </MenuItem>
                     </Menu>
                     <Menu iconShape="circle">
                         <SubMenu
-                            suffix={<span className="badge yellow">3</span>}
-                            icon={<FaRegLaughWink />}
+                            icon={<FaGem />}
+                            title="Features"
                         >
-                            <MenuItem> 1</MenuItem>
-                            <MenuItem> 2</MenuItem>
-                            <MenuItem> 3</MenuItem>
+                            <MenuItem> Quản lý User</MenuItem>
+                            <MenuItem> Quản lý Admin</MenuItem>
+                            <MenuItem> Quản lý bài thi</MenuItem>
                         </SubMenu>
 
 
@@ -72,14 +73,14 @@ const SideBar = (props) => {
                         }}
                     >
                         <a
-                            href="https://github.com/azouaoui-med/react-pro-sidebar"
+                            href="https://github.com/haanhtuan1710/React"
                             target="_blank"
                             className="sidebar-btn"
                             rel="noopener noreferrer"
                         >
                             <FaGithub />
                             <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
-                                viewSource
+                                haanhtuan1710
                             </span>
                         </a>
                     </div>
