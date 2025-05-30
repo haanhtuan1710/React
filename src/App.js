6618b20bae9afc3ab528938fb507a2ab102a0501
haanhtuan1710 <hatuan17102003@gmail.com>
2025-05-30 09:47:17 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
 
   return (
 
     <div className="app-container">
-      Hello World
+      <Header/>
 
     </div>
   );
