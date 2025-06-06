847c733bdb02adabb96a0abd32673e9863dbefa3
haanhtuan1710 <hatuan17102003@gmail.com>
2025-06-06 12:26:53 +0700

    tao base giao dien bai quiz

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,20 +1,73 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from 'lodash';
+import './DetailQuiz.scss'
+
 const DetailQuiz = () => {
-    const quizId = params.id;
+
+    const location = useLocation();
     const params = useParams();
+    const quizId = params.id;
 
-    useEffect(()=>{
+
+    useEffect(() => {
         fetchQuestions();
-    },[quizId]);
+    }, [quizId]);
 
-    const fetchQuestions = async()=>{
+    const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `id` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of items
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((index, item) => {
+                        if (index === 0) {
+                            questionDescription = item.questionDescription;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                }
+                )
+                .value();
+        }
     }
     return (
-        <div>
-            DetailQuiz
+        <div className="detail-quiz-container">
+            <div className="left-content">
+                <div className="title">
+                    Quiz{quizId}:{location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className="q-body">
+                    <img />
+                </div>
+                <div className="q-content">
+                    <div className="question">
+                        Question1: a?
+                    </div>
+                    <div className="answer">
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button className="btn btn-primary">Prev</button>
+                    <button className="btn btn-secondary">Next</button>
+                </div>
+
+            </div>
+            <div className="right-content">
+                count down
+            </div>
         </div>
 
     )
